3df16be26a05710d81b2e9089b92cd1fa4a51be8
nguyenquanghuy1999 <nguyenquanghuy1999@gmail.com>
2022-12-29 10:41:22 +0700

    commit

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,7 +1,8 @@
-const routes = {
+export const routes = {
     home: "/",
     following: "/following",
     profileUser: "/@:nickname",
     modalVideo: "/@:nickname/video/:id",
     live: "/live"
 }
+
